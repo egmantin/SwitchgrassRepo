f09bcd8338218d1ad7820bedec81ecc9ad148a79
egmantin <emily.mantin@smu.ca>
2020-06-26 13:50:43 -0300

    updated sg app for sharing

--- sg_app.R
+++ sg_app.R
@@ -1,4 +1,25 @@
 library(shiny)
+library(agricolae)
+library(lme4)
+library(nlme)
+library(boot)
+library(ggplot2)
+
+## Read in data from Excel.
+sg <- read.table("2020-05-13_SG_Model Data.csv", header = TRUE, sep = ",")
+
+## Standardization of data 
+meanYield <- mean(sg$Yield)
+sdYield <- sd(sg$Yield)
+zYield <- (sg$Yield - meanYield) / sdYield
+
+meanPrecip <- mean(sg$Precipitation)
+sdPrecip <- sd(sg$Precipitation)
+zPrecip <- (sg$Precipitation - meanPrecip) / sdPrecip
+
+meanGDD <- mean(sg$GDD)
+sdGDD <- sd(sg$GDD)
+zGDD <- (sg$GDD - meanGDD) / sdGDD
 
 ## Define the user interface ##
 ui <- fluidPage(
@@ -37,7 +58,7 @@ ui <- fluidPage(
       verbatimTextOutput("yield"),
     
       ## Output: Yield value ##
-      plotOutput("yield2")
+      textOutput("yield2")
       
       )
   )
@@ -46,13 +67,36 @@ ui <- fluidPage(
   
 
 ## Define server logic ##
+## Observe event - respond to Action button()
 server <- function(input, output) {
-  output$yield2 <- renderText({
+
+  observeEvent(input$calc, {
+  
+    ## Calculate zprecip value ##
+  
+    precip_z <- (input$precip - meanPrecip) / sdPrecip
+  
+    ## Calculate zGDD value ##
+  
+    GDD_z <- (input$gdd - meanGDD) / sdGDD
+  
+    ## Calculate zYield value ##
+  
+    zYield <- ((0.09657*precip_z)+(0.0101*precip_z^2)+(0.01519*GDD_z)-0.07127)
+  
+    ## Calculate predicted yield ##
+  
+    conYield <- ((zYield*sdYield)+meanYield)
     
   })
- 
+
+## Define ouputs ##  
+  
+  output$yield2 <- renderText({
+    paste("Predicted Yield =", conYield)
+    
+  })
 }
-
-
+  
 ## Run the application ##
 shinyApp(ui = ui, server = server)
